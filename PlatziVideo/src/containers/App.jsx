4898aafbd2a7b23fe301c0e04578168c43142890
David Delgado <dadch13.14@gmail.com>
2020-01-12 18:01:32 -0400

    Reto Custom Hooks

--- PlatziVideo/src/containers/App.jsx
+++ PlatziVideo/src/containers/App.jsx
@@ -14,6 +14,7 @@ const API = 'http://localhost:3000/initalState';
 
 const App = () => {
   const initialState = useInitialState(API);
+
   return initialState.lenght === 0 ? (
     <h4>Loading...</h4>
   ) : (
@@ -24,7 +25,10 @@ const App = () => {
       {initialState.mylist.length > 0 && (
         <Categories title='Mi lista'>
           <Carousel>
-            <CarouselItem />
+            {initialState.mylist.map((item) => (
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              <CarouselItem key={item.id} {...item} />
+            ))}
           </Carousel>
         </Categories>
       )}
@@ -39,7 +43,12 @@ const App = () => {
       </Categories>
 
       <Categories title='Originales de Platzi Video'>
-        <Carousel>{/* <CarouselItem /> */}</Carousel>
+        <Carousel>
+          {initialState.originals.map((item) => (
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
       </Categories>
 
       <Footer />
