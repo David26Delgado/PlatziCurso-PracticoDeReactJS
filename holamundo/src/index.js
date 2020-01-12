dca706635bdb38e3d9bfff070eed0daeb36d6ce5
David Delgado <dadch13.14@gmail.com>
2020-01-12 00:29:03 -0400

    Props

--- holamundo/src/index.js
+++ holamundo/src/index.js
@@ -5,8 +5,9 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 import HolaMundo from "./components/HolaMundo";
+import Button from "./components/Button";
 
-ReactDOM.render(<HolaMundo />, document.getElementById("root"));
+ReactDOM.render(<Button text="Click" />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
